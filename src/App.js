f27277a3cf78e8b2f76bc3e8ca76c81fa153b901
Vittori <s225105@studenti.polito.it>
2020-03-22 17:17:04 +0100

    new website

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import image from "./images/IMG_2130.JPG"
-import { SocialIcon } from 'react-social-icons';
-import {useTransition, animated} from 'react-spring';
 
 function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
 
-    const [items, set] = useState([{text: "David Vittori", key: 1}])
-    const transitions = useTransition(items, item => item.key, {
-        from: { transform: 'translate3d(0,-40px,0)', opacity: 0 },
-        enter: { transform: 'translate3d(0,0px,0)', opacity: 1 },
-        leave: { transform: 'translate3d(0,-40px,0)', opacity: 0 },
-    })
-    return (
+          <p className="App-bio" >hi, i'm david vittori a passionate and creative mobile developer based in italy.
+            i'm looking always for something cool and i love to help startups get an awesome app. <br/> <br/> just in case drop me a line.</p>
 
-        <div className="App" style={{backgroundImage: `url(${image})`, width: "100%", height: "100%"}}>
-            <header className="App-header">
-                <div style={{ padding: 20, marginTop: 30, borderRadius: 10}}>
-                    <div style={{display: "flex", flexDirection: "row"}}>
-
-                    {
-                        transitions.map(({ item, props, key }) =>
-                            <animated.p  className="App-Title" key={key} style={props}>{" " + item.text + " "}</animated.p>
-                        )
-                    }
-                    </div>
-
-                    <div style={{justifyContent: "center", alignItems: "flex-end", display: "flex"}}>
-
-                        <SocialIcon url="https://github.com/VittoriDavide" network={"github"} bgColor={"transparent"} fgColor={"black"}/>
-                        <SocialIcon url="https://www.linkedin.com/in/vittori/" bgColor={"transparent"} fgColor={"black"}/>
-                        <SocialIcon url="mailto:dvd.vittori@gmail.com" network={"email"} bgColor={"transparent"} fgColor={"black"}/>
-
-                    </div>
-
-                </div>
-
-            </header>
-
-
-        </div>
-    );
+          <div style={{flexDirection: "row", justifyContent: "space-between"}}>
+            <a
+                className="App-link"
+                href="https://github.com/VittoriDavide"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+                className="App-link"
+                href="https://www.linkedin.com/in/vittori/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
+            <a
+                className="App-link"
+                href="mailto:me@vittoridavid.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Email
+            </a>
+          </div>
+        </header>
+      </div>
+  );
 }
 
 export default App;
